f13151861e2dfe113eaf90a054d06540d3672b2c
Oktay Caylak <0riq4mi@gmail.com>
2023-05-29 04:20:05 +0300

    Ball Speed and Scoreboard fixed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
 import time
+SPEED = 0.1
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -24,7 +25,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,9 +33,11 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
+
     #Detect collision with r_paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
 
     #Detect R paddle misses
     if ball.xcor() > 380:
